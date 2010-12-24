f569d0d5641f46cc6d7d60b9adea72c981d16713
Wei Hu <weihu@cs.virginia.edu>
2010-12-24 02:50:18 -0500

    Add back the debs for users without access to Ubuntu

--- fake-debs/build-debs.sh
+++ fake-debs/build-debs.sh
@@ -5,5 +5,5 @@ names=(coreutils libgcc1 libstdc++6 libc6 libgmp3c2 zlib1g debianutils libncurse
 for name in ${names[@]}
 do
     ../tools/FakePkg $name
-    sudo dpkg -i "$name.deb"
+#    sudo dpkg -i "$name.deb"
 done
