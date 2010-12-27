19ff59089204c933db8e2ccae6a352ef2fe30752
Wei Hu <weihu@cs.virginia.edu>
2010-12-26 23:33:11 -0500

    Update fake debs for man-db and mlocate

--- fake-debs/build-debs.sh
+++ fake-debs/build-debs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-names=(coreutils libgcc1 libstdc++6 libc6 libgmp3c2 zlib1g debianutils libncurses5)
+names=(coreutils libgcc1 libstdc++6 libc6 libgmp3c2 zlib1g debianutils libncurses5 \
+       bsdutils libdb4.7 libbz2-1.0 libreadline6 base-files passwd)
 
 for name in ${names[@]}
 do
