3c0bd718fac5d9e002db16f885ca9dec281b5d3c
Wei Hu <weihu@cs.virginia.edu>
2010-12-27 00:12:49 -0500

    Update fake debs for x11-apps

--- fake-debs/build-debs.sh
+++ fake-debs/build-debs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 names=(coreutils libgcc1 libstdc++6 libc6 libgmp3c2 zlib1g debianutils libncurses5 \
-       bsdutils libdb4.7 libbz2-1.0 libreadline6 base-files passwd)
+       bsdutils libdb4.7 libbz2-1.0 libreadline6 base-files passwd \
+       ca-certificates libgcrypt11 libpam0g libpam-modules consolekit dbus libpng12-0 \
+       libsm6 libx11-6 libxcursor1 libxext6 libxft2 libxkbfile1 libxmu6 libxmuu1 libxrender1 libxt6 \
+       e2fsprogs libblkid1 libdbus-1-3 libuuid1 mount lsb-base ncurses-bin upstart x11-common\
+      )
 
 for name in ${names[@]}
 do
