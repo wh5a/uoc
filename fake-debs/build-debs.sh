054edb3878a1eeacf1d69fc4875f04dea05a80c2
Wei Hu <weihu@cs.virginia.edu>
2010-12-29 18:46:26 -0500

    Update fake debs for ghc6. Somehow Ubuntu insists on "upgrading" upstart if we assign it the same version number.

--- fake-debs/build-debs.sh
+++ fake-debs/build-debs.sh
@@ -6,6 +6,7 @@ names=(coreutils libgcc1 libstdc++6 libc6 libgmp3c2 zlib1g debianutils libncurse
        libsm6 libx11-6 libxcursor1 libxext6 libxft2 libxkbfile1 libxmu6 libxmuu1 libxrender1 libxt6 \
        e2fsprogs libblkid1 libdbus-1-3 libuuid1 mount lsb-base ncurses-bin upstart x11-common \
        file libasound2 libjpeg62 libmagic1 libncursesw5 libsqlite3-0 libssl0.9.8 \
+       libudev0 net-tools \
       )
 
 for name in ${names[@]}
